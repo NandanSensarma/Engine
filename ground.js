807b2673d2f8d4572c9f1ae6ac0dfaf8721a09fe
NandanSensarma <68332915+NandanSensarma@users.noreply.github.com>
2020-10-19 19:13:30 +0530

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,20 @@
+class Ground{
+    constructor(x,y,width){
+        var options={
+            isStatic:true
+                }
+    
+                this.body= Bodies.rectangle(x,y,width,10,options);
+                this.width=width;
+                this.height=10;
+    
+                World.add(world,this.body);
+    }
+    display (){
+        var pos=this.body.position;
+        rectMode(CENTER);
+        rect(pos.x,pos.y,this.width,this.height);
+        
+    } 
+    }
+    
